42c2a0c11a1154caabc38b19e63e516603fb0e66
Alexey Perebigaylo <dev_perebigaylo@starta.ru>
2023-02-14 11:41:13 +0700

    Попытки понять что происходит

---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import compileTemplate from "./profile/index.pug";
+
+const app = document.body;
+
+const pageProps = {
+  userData: [
+    ["Почта", "pochta@yan1dex.ru"],
+    ["Логин", "ivanivanov"],
+    ["Имя", "Иван"],
+    ["Фамилия", "Иванов"],
+    ["Имя в чате", "Иван"],
+    ["Телефон", "+7 (909) 967 30 30"],
+  ],
+  userImg: "https://i.pinimg.com/236x/14/47/14/144714db6b6c2ee70e77273691de5f50.jpg"
+}
+
+app.innerHTML = compileTemplate(pageProps);
+
+pageProps.userData[0][1] = "Почта";
+
+setTimeout(() => {
+  app.innerHTML = compileTemplate(pageProps);
+}, 2000);
