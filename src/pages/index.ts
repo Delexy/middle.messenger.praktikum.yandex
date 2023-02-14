3789c9c74a60a03998291651f13a98f32b001b81
Alexey Perebigaylo <dev_perebigaylo@starta.ru>
2023-02-14 14:11:16 +0700

    Донастройка TS

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -11,13 +11,13 @@ const pageProps = {
     ["Имя в чате", "Иван"],
     ["Телефон", "+7 (909) 967 30 30"],
   ],
-  userImg: "https://i.pinimg.com/236x/14/47/14/144714db6b6c2ee70e77273691de5f50.jpg"
-}
+  userImg: "https://i.pinimg.com/236x/14/47/14/144714db6b6c2ee70e77273691de5f50.jpg",
+};
 
-app.innerHTML = compileTemplate(pageProps);
+app.insertAdjacentHTML('beforeend', compileTemplate(pageProps));
 
 pageProps.userData[0][1] = "Почта";
 
 setTimeout(() => {
-  app.innerHTML = compileTemplate(pageProps);
+  app.insertAdjacentHTML('beforeend', compileTemplate(pageProps));
 }, 2000);
---
+++ typed.d.ts
@@ -0,0 +1,4 @@
+declare module '*.pug' {
+  const classes: function
+  export default classes
+}
