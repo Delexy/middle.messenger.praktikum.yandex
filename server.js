e4af91fe3287be04b48adc80300eb3710311eebb
Alexey Perebigaylo <dev_perebigaylo@starta.ru>
2023-03-13 14:31:19 +0700

    fix router

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const server = express();
 
 const PORT = process.env.PORT || 3000;
 server.use('/', express.static(__dirname + '/static'));
+server.get('*', (req, res) => {
+  res.sendFile(__dirname + '/static/index.html');
+});
 
 server.listen(PORT, () => {
   console.log(`Server start on ${PORT} port`);
